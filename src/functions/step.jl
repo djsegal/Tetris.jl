3ab2473e0e15335fe002ae430b36bd4826a795d0
djsegal <dansegal2@gmail.com>
2018-03-01 02:52:21 -0500

    Tighten up loss criterion (spawn or first drop)

--- src/functions/step.jl
+++ src/functions/step.jl
@@ -1,10 +1,18 @@
 function step(cur_player::AbstractPlayer)
-  if isnull(cur_player.piece)
-    summon_piece!(cur_player)
+  isnull(cur_player.piece) ||
+    return down(cur_player)
 
-    check(cur_player.piece, cur_player.grid) ||
-      raise_defeat(cur_player)
+  summon_piece!(cur_player)
+
+  if !check(cur_player.piece, cur_player.grid)
+    raise_defeat(cur_player)
+    return false
   end
 
-  down(cur_player)
+  if !down(cur_player)
+    raise_defeat(cur_player)
+    return false
+  end
+
+  true
 end
